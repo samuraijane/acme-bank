f6c55c63cbfdbc92342657d0309bf1d7a52ddb0e
Matthew Day <matt@matthewday.net>
2021-12-09 17:36:46 -0500

    remove create-react-app code we no longer need

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,6 @@
 import Footer from './sectioning/footer/footer';
 import Header from './sectioning/header/header';
 import Main from './sectioning/main/main';
-import './app.css';
 
 function App() {
   return (
